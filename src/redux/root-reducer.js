e5e56180df04cc180bac271ae8c457987c4aa536
sora0420 <thfk0420@naver.com>
2021-06-18 17:51:07 +0900

    0618 reception

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,14 +2,13 @@ import { combineReducers } from "redux";
 import createreceptionReducer from "./createReception-reducer";
 import inspectReducer from './inspection_Reducer';
 import diagnosisReducer from './diagnosis-reducer';
-
 import pastrecordReducer from './pastrecord-reducer';
 
 const rootReducer = combineReducers({
     inspectReducer,
     diagnosisReducer,
     pastrecordReducer,
-    createreceptionReducer
+    createreceptionReducer,
 });
 
 export default rootReducer;
