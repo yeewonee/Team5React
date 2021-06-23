55a23cb0773050356019ef74f526ac63d5c9e6f9
YUNHWAN <629jyh7@naver.com>
2021-06-23 17:56:44 +0900

    dignosis modal 분류

---
+++ src/views/Diagnosis/PastRecord/ModalPast.js
@@ -0,0 +1,65 @@
+import Modal from "./pastModal";
+import React from 'react'
+import style from "./pastrecord.module.css";
+import CommonTable from "views/table/CommonTable";
+import CommonTableRow from "views/table/CommonTableRow";
+import CommonTableColumn from "views/table/CommonTableColumn";
+
+export const ModalPast = (props) => {
+
+  const closeModal = () => {
+    props.closeModal();
+  };
+
+  return (
+    <>
+      {/* 과거기록 상세보기 modal */}
+      <Modal open={props.modalOpen} close={closeModal} header="검사결과 확인">
+        <div className={style.past_title}>환자 정보</div>
+        <CommonTable headersName={["환자번호", "환자이름", "주민번호"]}>
+          <CommonTableRow key={props.patient.patientId}>
+            <CommonTableColumn>{props.patient.patientId}</CommonTableColumn>
+            <CommonTableColumn>{props.patient.patientName}</CommonTableColumn>
+            <CommonTableColumn>{props.patient.patientSsn1}</CommonTableColumn>
+          </CommonTableRow>
+        </CommonTable>
+
+        <div className={style.past_title}>내원일 정보</div>
+        <CommonTable headersName={["내원일자"]}>
+          <CommonTableRow>
+            <CommonTableColumn>{props.pList[0]?.dDate}</CommonTableColumn>
+          </CommonTableRow>
+        </CommonTable>
+
+        <div className={style.past_title}>결과 확인</div>
+        <hr />
+
+        <div className={style.past_title2}>검사 결과</div>
+        <CommonTable headersName={["처방코드", "검사명", "검사담당자", "결과"]}>
+          {props.iResultList.map((iResultList, index) => (
+            <CommonTableRow key={iResultList.iId}>
+              <CommonTableColumn>{iResultList.iId}</CommonTableColumn>
+              <CommonTableColumn>{iResultList.iName}</CommonTableColumn>
+              <CommonTableColumn>{iResultList.inspector}</CommonTableColumn>
+              <CommonTableColumn>{iResultList.iResult}</CommonTableColumn>
+            </CommonTableRow>
+          ))}
+        </CommonTable>
+
+        <div className={style.past_title2}>약 처방</div>
+        <div className={style.past_title2}>검사 결과</div>
+        <CommonTable headersName={["코드", "명칭", "구분", "단위"]}>
+          {props.mResultList.map((mResultList, index) => (
+            <CommonTableRow key={mResultList.mId}>
+              <CommonTableColumn>{mResultList.mId}</CommonTableColumn>
+              <CommonTableColumn>{mResultList.mName}</CommonTableColumn>
+              <CommonTableColumn>{mResultList.mCategory}</CommonTableColumn>
+              <CommonTableColumn>{mResultList.mUnit}</CommonTableColumn>
+            </CommonTableRow>
+          ))}
+        </CommonTable>
+        
+      </Modal>
+    </>
+  )
+}
--- src/views/Diagnosis/PastRecord/index.js
+++ src/views/Diagnosis/PastRecord/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import style from "./pastrecord.module.css";
-import Modal from "./pastModal";
 import { useState } from "react";
 import CommonTable from "views/table/CommonTable";
 import { getPastRecord, getPatient, getResultIList, getResultMList } from "../data";
 import CommonTableRow from "views/table/CommonTableRow";
 import CommonTableColumn from "views/table/CommonTableColumn";
 import { useEffect } from "react";
+import { ModalPast } from "./ModalPast";
 
 export const PastRecord = (props) => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -28,9 +28,7 @@ export const PastRecord = (props) => {
 
   //환자 정보
   let patient = getPatient(props.patientId);
-
   let iResultList = getResultIList(props.patientId);
-
   let mResultList = getResultMList(props.patientId);
 
   return (
@@ -54,53 +52,15 @@ export const PastRecord = (props) => {
         <button className="btn btn-outline-dark mr-3">임시 저장</button>
       </div>
 
-      {/* 과거기록 상세보기 modal */}
-      <Modal open={modalOpen} close={closeModal} header="검사결과 확인">
-        <div className={style.past_title}>환자 정보</div>
-        <CommonTable headersName={["환자번호", "환자이름", "주민번호"]}>
-          <CommonTableRow key={patient.patientId}>
-            <CommonTableColumn>{patient.patientId}</CommonTableColumn>
-            <CommonTableColumn>{patient.patientName}</CommonTableColumn>
-            <CommonTableColumn>{patient.patientSsn1}</CommonTableColumn>
-          </CommonTableRow>
-        </CommonTable>
-
-        <div className={style.past_title}>내원일 정보</div>
-        <CommonTable headersName={["내원일자"]}>
-          <CommonTableRow>
-            <CommonTableColumn>{pList[0]?.dDate}</CommonTableColumn>
-          </CommonTableRow>
-        </CommonTable>
-
-        <div className={style.past_title}>결과 확인</div>
-        <hr />
-
-        <div className={style.past_title2}>검사 결과</div>
-        <CommonTable headersName={["처방코드", "검사명", "검사담당자", "결과"]}>
-          {iResultList.map((iResultList, index) => (
-            <CommonTableRow key={iResultList.iId}>
-              <CommonTableColumn>{iResultList.iId}</CommonTableColumn>
-              <CommonTableColumn>{iResultList.iName}</CommonTableColumn>
-              <CommonTableColumn>{iResultList.inspector}</CommonTableColumn>
-              <CommonTableColumn>{iResultList.iResult}</CommonTableColumn>
-            </CommonTableRow>
-          ))}
-        </CommonTable>
-
-        <div className={style.past_title2}>약 처방</div>
-        <div className={style.past_title2}>검사 결과</div>
-        <CommonTable headersName={["코드", "명칭", "구분", "단위"]}>
-          {mResultList.map((mResultList, index) => (
-            <CommonTableRow key={mResultList.mId}>
-              <CommonTableColumn>{mResultList.mId}</CommonTableColumn>
-              <CommonTableColumn>{mResultList.mName}</CommonTableColumn>
-              <CommonTableColumn>{mResultList.mCategory}</CommonTableColumn>
-              <CommonTableColumn>{mResultList.mUnit}</CommonTableColumn>
-            </CommonTableRow>
-          ))}
-        </CommonTable>
-        
-      </Modal>
+      <ModalPast 
+        patient={patient} 
+        iResultList={iResultList} 
+        mResultList={mResultList}
+        closeModal={closeModal}
+        modalOpen={modalOpen}
+        pList={pList}
+      />      
+     
     </div>
   );
 };
