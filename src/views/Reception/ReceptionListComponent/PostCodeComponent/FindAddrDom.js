406bcfbe799fcfd4dce34fd762150bc1f0d89594
sora0420 <thfk0420@naver.com>
2021-06-24 15:48:48 +0900

    0624 reception

---
+++ src/views/Reception/ReceptionListComponent/PostCodeComponent/FindAddrDom.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+ 
+const FindAddrDom = ({ children }) => {
+    const el = document.getElementById('FindAddrDom');
+    return ReactDOM.createPortal(children, el);
+};
+ 
+export default FindAddrDom;
