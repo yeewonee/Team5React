73956d3489a6f7ac4482d48ccf11033eb01b59f3
jeongyewon <yeewonee@naver.com>
2021-06-28 16:28:35 +0900

    login 수정~!

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,39 +1,41 @@
 import { Col, Container, Row } from "react-bootstrap";
 import style from "./login.module.css";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { createSetUser } from "redux/auth-reducer";
 
 function Login(props){
+  const dispatch = useDispatch();
+
+  const handleLogin = (event) => {
+    dispatch(createSetUser());
+  };
+
   return(
-    <div className={style.test}>
-    <Container fluid>
-    <Row>
-      <Col className={style.col1}></Col>
-    </Row>
-    <Row>
-      <Col className={style.col2}>
-        <h1>LOGIN</h1>
-      </Col>
-    </Row>
-    <Row>
-      <form className={style.box}>
-        <div className="form-group mt-3">
-          <label >병원 코드</label>
-          <input type="text" className="form-control"/>
-        </div>
-        <div className="form-group">
-          <label >User ID</label>
-          <input type="text" className="form-control"/>
-        </div>
-        <div className="form-group mt-3">
-          <label >Password</label>
-          <input type="text" className="form-control"/>
-        </div>
-        <div className={style.button}>
-          <button type="submit" className="btn btn-primary">Login</button>
-        </div>
-      </form>
-    </Row>
-    </Container>
+    <div className={style.bg}>
+      <div className={style.titleBox}><div className={style.titleContent}>Login</div></div>
+      <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        <form className={style.box}>
+          <div className="form-group mt-3">
+            <label >병원 코드</label>
+            <input type="text" className="form-control"/>
+          </div>
+          <div className="form-group">
+            <label >User ID</label>
+            <input type="text" className="form-control"/>
+          </div>
+          <div className="form-group mt-3">
+            <label >Password</label>
+            <input type="text" className="form-control"/>
+          </div>
+          <div className={style.button}>
+            <button type="submit" className="btn btn-danger mr-2"><Link to="/" style={{textDecoration:'none', color:'white'}}>Back</Link></button>
+            <button type="submit" className="btn btn-primary" onClick={handleLogin}>Login</button>
+          </div>
+        </form>
+      </div>
     </div>
+
   )
 }
 
