123f970939161cf8ffb587e8ccd5e48c1b6257db
jeongyewon <yeewonee@naver.com>
2021-06-29 11:06:04 +0900

    yewon commit

--- src/views/createReception/CheckTime/data.js
+++ src/views/createReception/CheckTime/data.js
@@ -1,9 +1,10 @@
 import moment from "moment";
 
 let data = [
-  {r_id: 1, r_date: "2021-06-23", r_time: "9:00", r_status: "접수대기", r_role: "예약접수", patient_id: '04928', doctor_id: '1'},
-  {r_id: 2, r_date: "2021-06-23", r_time: "9:30", r_status: "예약완료", r_role: "예약접수", patient_id: '12346', doctor_id: '1'},
-  {r_id: 2, r_date: "2021-06-24", r_time: "9:30", r_status: "예약완료", r_role: "예약접수", patient_id: '12347', doctor_id: '1'}
+  {r_id: 3, r_date: "2021-06-28", r_time: "10:30", r_status: "접수대기", r_role: "예약접수", patient_id: '32319', doctor_id: '1'},
+  {r_id: 2, r_date: "2021-06-29", r_time: "9:30", r_status: "예약완료", r_role: "예약접수", patient_id: '12346', doctor_id: '1'},
+  {r_id: 1, r_date: "2021-06-30", r_time: "9:30", r_status: "예약완료", r_role: "예약접수", patient_id: '12346', doctor_id: '1'},
+  {r_id: 4, r_date: "2021-06-30", r_time: "10:30", r_status: "예약완료", r_role: "예약접수", patient_id: '12347', doctor_id: '1'}
 ];
 
 export function getReceptionDate(doctor_id, r_date){
