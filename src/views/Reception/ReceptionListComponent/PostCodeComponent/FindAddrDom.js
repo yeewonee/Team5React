e018d54d4569952a877f3d57965f6042b3b4977e
Jeongyewon <dbd1205@naver.com>
2021-07-13 17:09:46 +0900

    yewon commit

--- src/views/Reception/ReceptionListComponent/PostCodeComponent/FindAddrDom.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
- 
-const FindAddrDom = ({ children }) => {
-    const el = document.getElementById('FindAddrDom');
-    return ReactDOM.createPortal(children, el);
-};
- 
-export default FindAddrDom;
