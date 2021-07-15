80f33d3401769cbbdbd544967314cc3477cbc8f0
YUNHWAN <629jyh7@naver.com>
2021-07-15 11:23:50 +0900

    chat 수정

--- src/ModalChat.js
+++ src/ModalChat.js
@@ -1,12 +1,7 @@
 import Modal from "../src/views/Diagnosis/PastRecord/Modal";
-import React, { useEffect, useRef, useState } from 'react'
-import CommonTable from "views/table/CommonTable";
-import CommonTableColumn from "views/table/CommonTableColumn";
-import { getUserList, sendMqttMessage } from "apis/diagnosis";
-import CommonTableRow from "views/table/CommonTableRow";
+import React from 'react'
+import { ChatRoom } from "ChatRoom";
 import { useSelector } from "react-redux";
-import Paho from "paho-mqtt";
-import style from "./chat.module.css";
 
 
 
@@ -20,149 +15,6 @@ export const ModalChat = (props) => {
     return state.authReducer.uid;
   });
 
-  const [user, setUser] = useState([]);
-  const userList = async () => {
-    try {
-      const response = await getUserList(uid);
-      setUser(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    userList();
-  }, []);
-
-  const [pubState, setPubState] = useState(false);
-  const handleUserName = (event, userId) => {
-    setColorSelect(userId)
-    console.log("들어옴" + userId)
-    setPubMessage((prev) => {
-      return {
-        ...prev,
-        topic:  "/topic1/" + userId
-      }
-    });
-    setPubState(true);
-    setSubTopic("/topic1/" + uid)
-  }
-  
-  
-   //-------------------------------------------------------------  
-  //상태 선언
-  //-------------------------------------------------------------
-  const [connected, setConnected] = useState(false);
-  const [subTopic, setSubTopic] = useState("/topic1/" + uid);
-  const [prevSubTopic, setPrevSubTopic] = useState("/topic1/#");
-  const [pubMessage, setPubMessage] = useState({
-    topic: "/topic1/#",
-    content: "Hello"
-  });
-
-  const [contents, setContents] = useState([]);
-  const [sendContents, setSendContents] = useState([]);
-
-  //입력 양식 값이 변경될 때 상태 변경
-
-  const changePubMessage = (event) => {
-
-    setPubMessage((prev)=>{
-      return{
-        ...prev,
-        [event.target.name]:event.target.value
-      }
-    });
-  };
-
-
-
-  //-------------------------------------------------------------
-  //버튼 이벤트 처리
-  //-------------------------------------------------------------
-  let client = useRef(null);
-  const connectMqttBroker = () => {
-    //Paho.MQTT.Clinet에서 MQTT가 빠짐
-    client.current = new Paho.Client("localhost", 61614, "client-" + new Date().getTime());
-
-    client.current.onConnectionLost = () => {
-      console.log("접속 끊김");
-      setConnected(false);
-    };
-
-    client.current.onMessageArrived = (msg) => {
-      let today = new Date();   
-      let hours = today.getHours(); // 시
-      let minutes = today.getMinutes();  // 분
-      console.log("메시지 수신");
-      var message = JSON.parse(msg.payloadString);
-      setContents((contents) => {
-        return contents.concat(message.content);
-      });
-
-      setSendContents((contents) => {
-        return contents.concat("시간" + hours + ':' + minutes);
-      });
-    };
-
-    client.current.connect({onSuccess:() => {
-      console.log("접속 성공");
-      setConnected(true);
-      sendSubTopic();
-    }});
-  };
-
-  const disconnectMqttBroker = () => {
-    client.current.disconnect();
-  };
-
-  const sendSubTopic = () => {
-    client.current.unsubscribe(prevSubTopic);
-    client.current.subscribe(subTopic);
-
-    setPrevSubTopic(subTopic);
-
-  }
-
-  const publishTopic = async () => {
-    let today = new Date();   
-    let hours = today.getHours(); // 시
-    let minutes = today.getMinutes();  // 분
-
-    setSendContents((contents) => {
-      return contents.concat(pubMessage.content);
-    });
-
-    setContents((contents) => {
-      return contents.concat("시간" + hours + ':' + minutes);
-    });
-
-    await sendMqttMessage(pubMessage);
-
-  } 
-
-  //-------------------------------------------------------------
-  //마운트 및 언마운트에 실행할 내용
-  //-------------------------------------------------------------
-  useEffect(() => {
-    connectMqttBroker();
-    return (() => {
-      disconnectMqttBroker();
-    });
-  }, []);
-
-const check = (content) => {
-  let temp = content.charAt(0);
-  if(temp === '시'){
-    return true;
-  }else{
-    return false;
-  }
-
-}
-
-const [colorSelect, setColorSelect] = useState("");
-
   return (
     <>
       {/* 과거기록 상세보기 modal */}
@@ -173,7 +25,9 @@ const [colorSelect, setColorSelect] = useState("");
           <div style={{border:'1px solid black', width:'70%', marginLeft:'10px'}}>채팅방</div>
         </div>
 
-        <div className="d-flex justify-content-between" style={{marginTop:'10px'}}>
+        <ChatRoom uid={uid}/>
+
+        {/* <div className="d-flex justify-content-between" style={{marginTop:'10px'}}>
           <div style={{border:'1px solid black', width:'30%', height:'45vh'}}>
           <CommonTable headersName={["이름"]} tstyle={"table table-sm"}>
             {user.map((user, index) => (
@@ -208,8 +62,7 @@ const [colorSelect, setColorSelect] = useState("");
             <p style={{marginLeft:'38%', marginTop:'30%'}}>상대를 선택하세요.</p>
           </div>
           }
-         
-
+      
         </div>
         
         <div style={{width:'69%', marginLeft:'31%'}}>
@@ -219,7 +72,7 @@ const [colorSelect, setColorSelect] = useState("");
               <button class="btn btn-secondary" type="button" onClick={publishTopic}>전송</button>
             </div>
           </div>
-        </div>
+        </div> */}
        
         
       </Modal>
