54b2becd51357a76c86ffb3c127a4714aeb32ee3
YUNHWAN <629jyh7@naver.com>
2021-07-21 16:46:20 +0900

    diagnosis 수정

--- src/apis/diagnosis.js
+++ src/apis/diagnosis.js
@@ -1,40 +1,40 @@
 import axios from "axios";
 
 export function getMedicineList(){
-  const promise = axios.get("/diagnosis/medicines");
+  const promise = axios.get("/api/diagnosis/medicines");
   return promise;
 }
 
 export function getInspectList(){
-  const promise = axios.get("/diagnosis/inspectionlist")
+  const promise = axios.get("/api/diagnosis/inspectionlist")
   return promise;
 }
 
 export function getInspectAllList(){
-  const promise = axios.get("/diagnosis/inspections")
+  const promise = axios.get("/api/diagnosis/inspections")
   return promise;
 }
 
 export function getPatientList(day){
-  const promise = axios.get("/diagnosis/patientlist", {params:{day}})
+  const promise = axios.get("/api/diagnosis/patientlist", {params:{day}})
   return promise;
 }
 
 export function getPastRecordList(pId){
-  const promise = axios.get("/diagnosis/pastrecord", {params:{pId}})
+  const promise = axios.get("/api/diagnosis/pastrecord", {params:{pId}})
   return promise;
 }
 
 export function getPatient(pId){
   if(pId !== ""){
-    const promise = axios.get("/diagnosis/selectpatient", {params:{pId}})
+    const promise = axios.get("/api/diagnosis/selectpatient", {params:{pId}})
     return promise;
   }
   
 }
 
 export function getPastIlist(pId, day){
-  const promise = axios.get("/diagnosis/pastrecordilist", {params:{pId, day}})
+  const promise = axios.get("/api/diagnosis/pastrecordilist", {params:{pId, day}})
   return promise;
 }
 
